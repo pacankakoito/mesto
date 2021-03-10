656584a03549ed9a3eac30599cbbefa181bbb4a7
Pavel Kozlov <pacanakakoito@yandex.ru>
2021-03-10 21:01:00 +0300

    feat: сделать форму и алгоритм добавления новых карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,112 @@
-let editButton = document.querySelector('.profile__edit-button');
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+let editButton = document.querySelector('.profile__edit-button');//нашли кнопку изменить
+let addButton = document.querySelector('.profile__add-button');//нашли кнопку добавить
 let popup = document.querySelector('.popup');
-let closePopupButton = document.querySelector('.popup__close-button');
+let popupEdit = document.querySelector('.popup_type_edit');
+let popupAdd = document.querySelector('.popup_type_add');
+let closePopupEditButton = document.querySelector('#popup-edit__close-button');
+let closePopupAddButton = document.querySelector('#popup-add__close-button');
 
 // Находим форму в DOM
-let formElement=document.querySelector('.edit-form');// Воспользуйтесь методом querySelector()
-let nameInput=formElement.querySelector('.edit-form__input_type_name');// Воспользуйтесь инструментом .querySelector()
-let jobInput=formElement.querySelector('.edit-form__input_type_job');// Воспользуйтесь инструментом .querySelector()
+
+
+let formEdit=document.querySelector('#form-edit');
+let nameInput=formEdit.querySelector('.form-edit__input_type_name');// Воспользуйтесь инструментом .querySelector()
+let jobInput=formEdit.querySelector('.form-edit__input_type_job');// Воспользуйтесь инструментом .querySelector()
 // Находим профиль в DOM
+let formAdd=document.querySelector('#form-add');
+let titleInput=formAdd.querySelector('.form-add__input_type_title');// Воспользуйтесь инструментом .querySelector()
+let linkInput=formAdd.querySelector('.form-add__input_type_link');// Воспользуйтесь инструментом .querySelector()
+
 let profile=document.querySelector(".profile"); // Воспользуйтесь инструментом .querySelector()
 let profileName=profile.querySelector(".profile__title"); // Воспользуйтесь инструментом .querySelector()
 let profileJob=profile.querySelector(".profile__subtitle");// Воспользуйтесь инструментом .querySelector()
 
-//функция открытия попапа: слушатель определяет клик и подмиксовывает селектор
-function showPopup(){
-    popup.classList.add('popup_opened');
+
+
+let elementList = document.querySelector('.elements__list');
+
+function createCard(name, link) {
+    const templateElement = document.querySelector('#template').content;//нашли темплейт
+    const card = templateElement.cloneNode(true);//клонировали ноду
+    card.querySelector('.element__image').src = link;//присвоили значения
+    card.querySelector('.element__image').alt = name;
+    card.querySelector('.element__title').textContent = name;
+    elementList.append(card);
+  }
+  function initElements(arr) {//инициализировали заполнение
+    arr.forEach(elem => {
+      createCard(elem.name, elem.link);
+    });
+  }
+
+function openEditPopup() { //открытие попапа для редактирования
     nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+    jobInput.value = profileJob.textContent; // Вставьте новые значения с помощью textContent
+    popupEdit.classList.add('popup_opened');
+}
+function openAddPopup(){
+    popupAdd.classList.add('popup_opened');
 }
 
-//функция закрытия попапа: слушатель определяет клик и подмиксовывает селектор
-function closePopup(){
-    popup.classList.remove('popup_opened');
-}
 
-
-
-function formSubmitHandler (evt) {
+function formEditSubmitHandler (evt) {
     evt.preventDefault();
-     profileName.textContent = nameInput.value;
-     profileJob.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
-     closePopup();//закрыли попап после сохранения
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
+    closePopupEdit(); //закрыли попап после сохранения
 }
- 
+function formAddSubmitHandler (evt) {
+    evt.preventDefault();
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+    createCard(titleInput.value, linkInput.value, 'apppend');
+    closePopupAdd(); //закрыли попап после сохранения
+}
 
-editButton.addEventListener('click', showPopup);
-closePopupButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+
+function closePopupEdit(){
+    closePopup(popupEdit);
+}
+function closePopupAdd(){
+    closePopup(popupAdd);
+}
+function closePopup(element){ 
+    element.classList.remove('popup_opened');//функция для закрытия попапов
+
+ }
+
+editButton.addEventListener('click', openEditPopup);
+addButton.addEventListener('click', openAddPopup);
+closePopupEditButton.addEventListener('click', closePopupEdit);
+closePopupAddButton.addEventListener('click', closePopupAdd);
+
+formEdit.addEventListener('submit', formEditSubmitHandler);
+formAdd.addEventListener('submit', formAddSubmitHandler);
+
+initElements(initialCards);
